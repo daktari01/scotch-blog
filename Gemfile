2a9ab59082b93ff8ba72bfd1a5f5527d3f5093c0
Daktari <dindijjames@gmail.com>
2017-12-28 12:21:24 +0000

    Set up image upload

--- Gemfile
+++ Gemfile
@@ -54,3 +54,7 @@ gem 'simple_form', '~> 3.2'
 gem 'bootstrap-sass', '~> 3.3'
 gem 'ckeditor', '~> 4.1'
 gem 'devise'
+
+# Image upload
+gem 'carrierwave'
+gem 'mini_magick'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171228112240) do
+ActiveRecord::Schema.define(version: 20171228115711) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
